304c48ff361dbaf2887ada68cf2939d491bcb5c9
Loc <loc@Zipcoders-MacBook-Pro-3.local>
2023-11-21 00:37:18 -0500

    finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -8,7 +8,45 @@ public class Music {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+        int countUp = getCountUp(startIndex, selection);
+        int countDown = getCountDown(startIndex, selection);
+        if (countUp < countDown) {
+            return countUp;
+        } else {
+            return countDown;
+        }
+    }
+
+    private int getCountDown(Integer startIndex, String selection) {
+        int countDown = 0;
+        for (int i = startIndex; i >= 0; i--) {
+            if (!playList[i].equals(selection)) {
+                if (i == 0) {
+                    for (int j = playList.length - 1; j > startIndex; j--){
+                     if (!playList[j].equals(selection)) {
+                         countDown++;
+                         //System.out.println(countDown + " before zero");
+                     }
+                    }
+                } else {
+                    countDown++;
+                    //System.out.println(countDown);
+                }
+            }
+        }
+        System.out.println("total countdown "+countDown);
+        return countDown;
+    }
+
+    private int getCountUp(Integer startIndex, String selection) {
+        int countUp = 0;
+        for (int i = startIndex; i < playList.length; i++) {
+            if (!playList[i].equals(selection)) {
+                countUp++;
+            }
+        }
+        System.out.println("total countup " +countUp);
+        return countUp;
     }
 }
